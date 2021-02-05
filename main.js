31460252b4aa7cc381909b54628fd1293d996d5f
babulakterfsd <babulakterfsd@gmail.com>
2021-02-05 19:57:58 +0600

    changed

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 'use strict';
 
+let processOrder = () => {
+    console.log(`Order is being processed`);
+    setTimeout()
+    console.log(`Order has been processed and served`);
+}
+
+console.log(`Customer places the order`);
+processOrder();
+console.log(`Customer has had the meal and paid the bill`);
