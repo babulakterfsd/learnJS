2e322214bb8ea774eb800a6517c9eb07148d1a5e
babulakterfsd <babulakterfsd@gmail.com>
2021-02-08 10:25:27 +0600

    h

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 'use strict';
 
-let processOrder = () => {
-    console.log(`Order is being processed`);
-    setTimeout()
-    console.log(`Order has been processed and served`);
+let completeOrder = () => {
+  console.log(`chef satrts cooking food`);
+  setTimeout(() => {
+     console.log(`Food is being prepared to be served`);
+  },3000)
+  console.log(`food has been served`);
 }
 
 console.log(`Customer places the order`);
-processOrder();
-console.log(`Customer has had the meal and paid the bill`);
+
+completeOrder();
+
+console.log(`customer paid the bill and goes out`);
