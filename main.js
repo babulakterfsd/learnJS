f6c84188d5e65385f187eafa53bb08a1097bb453
babulakterfsd <babulakterfsd@gmail.com>
2021-02-08 15:21:29 +0600

    changed

--- main.js
+++ main.js
@@ -1,15 +1,2 @@
 'use strict';
 
-let completeOrder = () => {
-  console.log(`chef satrts cooking food`);
-  setTimeout(() => {
-     console.log(`Food is being prepared to be served`);
-  },3000)
-  console.log(`food has been served`);
-}
-
-console.log(`Customer places the order`);
-
-completeOrder();
-
-console.log(`customer paid the bill and goes out`);
