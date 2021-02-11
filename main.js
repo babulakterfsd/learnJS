3ef09e87820b7a0c7edff91703c6dc8e1c83117d
babulakterfsd <babulakterfsd@gmail.com>
2021-02-11 11:24:50 +0600

    hello

--- main.js
+++ main.js
@@ -1,2 +1,28 @@
 'use strict';
 
+let getReq = (method,url,callback) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open(method,url);
+    xhr.onreadystatechange = (e) => {
+        if(xhr.readyState == 4) {
+            if(xhr.status == 200) {
+                callback(null,xhr.response)
+            } else {
+                callback(xhr.status,null)
+            }
+        }
+    }
+    xhr.send()
+}
+
+getReq(`get`,`https://jsonplaceholder.cypress.io/todos`,(err,res) => {
+    if(err) {
+        console.log(err);
+    } else {
+        let result = JSON.parse(res)
+        for(let i = 10; i < 15; i++) {
+            console.log(result[i].id);
+            console.log(result[i].title);
+        }
+    }
+})
