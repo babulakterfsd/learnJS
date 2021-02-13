7d169195d46c716ac2dcda895ba45bc638c49930
babulakterfsd <babulakterfsd@gmail.com>
2021-02-13 23:20:59 +0600

    message

--- main.js
+++ main.js
@@ -1,18 +1,2 @@
 'use strict';
 
-// let mypromise = new Promise((resolve) => {
-//     setTimeout(() => [
-//        resolve(`data connected`)
-//     ],3000)
-// })
-
-// async function hello() {
-//     console.log(`first line`);
-    
-//     let res = await mypromise;
-//     console.log(res);
-
-//     console.log(`last line`);
-// }
-
-// hello()
