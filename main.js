428a1c9bba51100a2040e02e218e6649891d7833
babulakterfsd <babulakterfsd@gmail.com>
2021-02-13 19:27:34 +0600

    this message has been sent to server

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 'use strict';
 
+// let mypromise = new Promise((resolve) => {
+//     setTimeout(() => [
+//        resolve(`data connected`)
+//     ],3000)
+// })
+
+// async function hello() {
+//     console.log(`first line`);
+    
+//     let res = await mypromise;
+//     console.log(res);
+
+//     console.log(`last line`);
+// }
+
+// hello()
