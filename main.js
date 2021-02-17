f00a38d53bd2eab79069e4f74ce2ef97b2e4443a
babulakterfsd <babulakterfsd@gmail.com>
2021-02-17 16:02:35 +0600

    changed

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 'use strict';
 
+let changer = (id) => {
+    let target = document.getElementById(id);
+    let i = 200;
+    let stopper = setInterval(() => {
+       i += 1;
+       if(i == 700) {
+           clearInterval(stopper)
+       } else {
+           target.style.width = i + `px`;
+       }
+    },0)
+}
+
+document.getElementById(`mybtn`).addEventListener((`click`),() => {
+    changer(`myanimation`)
+})
